95b03362f1a8ebe5a0317b61ec0da37863f3c36f
arygupta04 <157335259+arygupta04@users.noreply.github.com>
2025-07-10 08:37:19 +1000

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,11 @@
-import os
 import discord
 from dotenv import load_dotenv
-import tensorflow as tf
-from tensorflow.keras.layers import TextVectorization
-import numpy as np
-import pandas as pd
 from toxicity_wrapper import ToxicityClassifier
 
 # This function loads environment variables from .env
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'MTMyNjQzMTc1ODc1Njg3MjIzNA.GNnUcn.mtEQCyMNP2SjqqXweIoLifKrcb2cZxH5-a5dTQ'
 MAX_FEATURES = 200000 # number of words in the vocab
 
 
@@ -21,15 +16,10 @@ intents.message_content = True  # Allow access to message content
 
 client = discord.Client(intents=intents)
 
-# Initialize the ToxicityClassifier with model paths
-model_paths = {
-    'toxic': 'models/model_toxic_4.keras',
-    'obscene': 'models/model_obscene_4.keras',
-    'threat': 'models/model_threat_4.keras',
-    'insult': 'models/model_insult_4.keras',
-    'identity hate': 'models/model_identity_hate_4.keras'
-}
-classifier = ToxicityClassifier(model_paths=model_paths)
+# Initialize the ToxicityClassifier with model path
+model_path = 'best_model.h5'
+
+classifier = ToxicityClassifier(model_path=model_path)
 
 
 # This event is triggered when the bot successfully connects to Discord
@@ -45,14 +35,12 @@ async def on_ready():
     if channel:
         await channel.send("Hello! I'm monitoring for inappropriate comments.")
 
-
 # This event is triggered when a message is sent in a channel the bot can access
 @client.event
 async def on_message(message):
     # Prevent the bot from responding to its own messages
     if message.author == client.user:
         return
-
 
     feedback = classifier.classify(message.content)
     if feedback:
